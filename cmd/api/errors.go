816504824f766fb2e5a6a5f7303d9a269ab13334
Zack Leitzel <zackleitzel@gmail.com>
2024-07-14 19:37:55 -0500

    feat: add error response helpers

---
+++ cmd/api/errors.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Generic helper for logging an error message along with the
+// current request method and URL as attributes in the log entry.
+func (app *application) logError(r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+// Generic helper for sending JSON-formatted error messages to the client with a given status code.
+// Note that were using the any type for the message parameter, rather than just a string type, as this
+// gives us more flexibility over the values that we can include in the response.
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+// The serverErrorResponse() method will be used when our application encounters an unexpected
+// problem at runtime. It logs the detailed error message, then uses the errorResponse() helper
+// to send a 500 Internal Server Error status code and JSON response (containing a generic error message)
+// to the client.
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
